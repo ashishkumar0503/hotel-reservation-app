5784cfed6a43d603df7a194d3a8c3475cd91943d
Ashish Kumar <kumarashish97713@gmail.com>
2023-03-28 11:53:22 +0530

    created login page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Hotel from './pages/hotel/Hotel';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path='/' element={<Login/>} />
+        <Route path="/home" element={<Home/>}/>
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
       </Routes>
